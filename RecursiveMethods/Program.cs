28a597fbeba6b46010c43dd5cede0c129e75dd61
Fredrik Johansson <fredrik@everyloop.com>
2020-09-18 14:58:39 +0200

    Add demo of recursive methods

---
+++ RecursiveMethods/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace RecursiveMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program starts here");
+            Method1();
+            Console.WriteLine("Program ends here");
+        }
+
+        static void Method1()
+        {
+            Console.WriteLine("Method 1 starts here");
+            Method2(0);
+            Console.WriteLine("Method 1 ends here");
+        }
+
+        static void Method2(int x)
+        {
+            x++;
+            //Console.WriteLine($"x = {x}");
+            if (x > 10000)
+            {
+                Console.WriteLine(x);
+            }
+            Method2(x);
+        }
+    }
+}
---
+++ StringExtensions/StringExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StringExtensions
+{
+    public static class StringExtensions
+    {
+        public static string Title(this string text)
+        {
+            return text.Substring(0, 1).ToUpper() + text.Substring(1).ToLower();
+        }
+    }
+}
