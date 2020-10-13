86829209087fc50551d2c9bd840a8d482bfba1a3
Fredrik Johansson <fredrik@everyloop.com>
2020-10-13 15:12:04 +0200

    Add UserControlDemo

---
+++ UserControlDemo/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UserControlDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            MyControl myControl = new MyControl();
+            splitContainer1.Panel2.Controls.Add(myControl);
+            myControl.Dock = DockStyle.Fill;
+
+            myControl.ButtonClicked += MyControl_ButtonClicked;
+
+            /*
+            TreeNode root = new TreeNode("Root node");
+            TreeNode root2 = new TreeNode("Root node 2");
+            treeView1.Nodes.Add(root);
+            treeView1.Nodes.Add(root2);
+
+            root.Nodes.Add("fredrik");
+            */
+        }
+
+        private void MyControl_ButtonClicked(object sender, AddNodeEventArgs e)
+        {
+            if (treeView1.SelectedNode == null)
+            {
+                treeView1.Nodes.Add(e.NodeName);
+            }
+            else
+            {
+                treeView1.SelectedNode.Nodes.Add(e.NodeName);
+                treeView1.SelectedNode.Expand();
+            }
+
+            treeView1.Focus();
+        }
+    }
+}
---
+++ UserControlDemo/MyControl.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UserControlDemo
+{
+    public class AddNodeEventArgs : EventArgs
+    {
+        public string NodeName { get; set; }
+
+        public AddNodeEventArgs(string name)
+        {
+            NodeName = name;
+        }
+    }
+    public partial class MyControl : UserControl
+    {
+        //public delegate void MyEventHandler(object sender, AddNodeEventArgs args);
+        
+        public event EventHandler<AddNodeEventArgs> ButtonClicked;
+
+        public MyControl()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ButtonClicked?.Invoke(this, new AddNodeEventArgs(comboBox1.Text));
+
+            if (!comboBox1.Items.Contains(comboBox1.Text))
+            {
+                comboBox1.Items.Add(comboBox1.Text);
+            }
+        }
+    }
+}
